e1b0859c0f74f701bc1900550a42d9eeb345d4d4
taufikps <82429783+taufikps@users.noreply.github.com>
2021-06-23 13:10:14 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
+$host = "projectmap.mysql.database.azure.com";
+$user = "toc@projectmap";
+$password = "Techno123";
 $database = "map";
 
 $conn = mysqli_connect($host,$user,$password,$database);
