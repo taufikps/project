4833318338c0ec399f7520fb7a0f5ed498475df0
taufikps <82429783+taufikps@users.noreply.github.com>
2021-06-27 19:20:50 +0700

    Update data.php

--- data.php
+++ data.php
@@ -64,7 +64,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Image Map By Taufik</title>
+    <title>Image Map By Firdy</title>
     <meta name="author" content="Codeconvey" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
